1a24223e2c7406a37c9931ed9ecc460397955afa
Francesco Montesano <franz.bergesund@gmail.com>
2017-10-12 22:16:50 +0200

    make even more basic, add some more info

--- sphinx_issue/module.py
+++ sphinx_issue/module.py
@@ -1,17 +1,19 @@
 '''Module documentation.
 
-* :attr:`errors`: some attribute.
+* This code raises a ``TypeError`` coming from re.py. See log
+* If ``_Errors.__getattr__`` returns something else, e.g. a string, the
+  documentation builds
+* If the ``_Errors`` instance is assigned to a private variable an underscore,
+  the documentation builds and the variable is not shown, despite using
+  :private-members:
 '''
-a_module = None
 
 
 class _Errors(object):
+    "some doc"
     def __getattr__(self, name):
-        if a_module:
-            return getattr(a_module, name)
-        else:
-            return Exception
+        return ValueError
 
 
-errors = _Errors()
+variable = _Errors()
 '''A test'''
